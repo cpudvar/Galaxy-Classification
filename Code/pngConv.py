cb766a37c85da04a2fff3f7d13b59709a5d3e113
Madros <A.madros@gmail.com>
2014-03-04 13:24:16 -0500

    Changed to log

--- Code/pngConv.py
+++ Code/pngConv.py
@@ -3,6 +3,6 @@ import f2n
 myimage = f2n.fromfits("name.fits")
 
 myimage.setzscale()
-myimage.makepilimage("lin", negative = False)
+myimage.makepilimage("log", negative = False)
 
 myimage.tonet("file.png")
