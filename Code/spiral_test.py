e9663dc80b3ae4de707d8f2451b95d75ec7bf6d2
cpudvar <kpud1337@gmail.com>
2014-03-31 19:24:11 -0400

    cleaned content, commented, renamed variables

--- Code/spiral_test.py
+++ Code/spiral_test.py
@@ -4,90 +4,95 @@ import scipy
 import matplotlib
 import pylab
 import os
-from scipy import ndimage
 import sys
+from scipy import ndimage
+from pngConv import *
 
 def main():
-    imageName = "658nmos.fits"
+    imageName = "red.fits"
+    
+    # for testing, save image data as .png for viewing
+    convert(imageName)    
+        
     #We should make this non-hardcoded in ANY way --> Caleb agrees
-    ###
+    
     #if (len(sys.argv)!=2):
         #imageName = sys.argv[1]
-    ###
+        
     #read in FITS file, find midpoint
     #Get location of script because path is basically
     #  hardcoded in. Find a better way?
-    os.chdir(os.path.dirname(sys.argv[0]))
+    #os.chdir(os.path.dirname(sys.argv[0]))
     imageFileLocation = os.path.abspath(os.path.join(os.pardir, 'images'))
-    print imageFileLocation
-    #from location of python script
+    
+    print imageFileLocation #from location of python script  
     
     #header data unit    
     hdulist = fits.open(os.path.join(imageFileLocation, imageName))
-    #print hdulist.info()
+    print hdulist.info()
     
     imageData = hdulist[0].data
     
     # TODO: make sure this works for all images
     dimensions = getDimensions(hdulist["PRIMARY"])  
-    brightest = blurring(imageData)
+    #brightest = blurring(imageData)
     
-    minValue, maxValue, pixelValues = findPixelRange(imageData, dimensions)
+    minValue, maxValue, horizontalMidpointArray, verticalMidpointArray = findPixelRange(imageData, dimensions)
     print "Min value: " , minValue , "\nMax value: " , maxValue
-    print "brightest", brightest
-    plotTable(pixelValues)
-
+    #print "brightest", brightest
+    
+    plotTable(horizontalMidpointArray)
+    plotTable(verticalMidpointArray)
     hdulist.close()
     
 def getDimensions(data):    
     return data.shape
     
-def plotTable(yData):
-    numPoints = len(yData)
-    xData = []
-    
-    for i in range(numPoints):
-        xData.append(i+1)        
-    
-    entries = len(xData)
-    
-    #view vales that are plotted
-    """
-    for i in range(entries):
-        print xData[i], yData[i]
-    """
-    
-    matplotlib.pyplot.scatter(xData, yData)    
-    pylab.show()
-    
 def findPixelRange(fileName, dimensions):
-    minValue=9999
-    maxValue=0
-    pixelValues = []
+    minValue = 999999
+    maxValue = -999999
+    horizontalMidpointArray = []
+    verticalMidpointArray = []
     
+    #retrieve height and width of image
     width = dimensions[0]
     height = dimensions[1]
     
-    #find midpt of height -> values for horizontal line
-    y = height/2
-
-    """
+    #find midpoint values of image
+    horizontalMidpoint = height/2
+    verticalMidpoint = width/2
+    
     #finds range of pixel values for a given image (min and max)
-    for x in range(1600):
-        for y in range(1600):
+    for x in range(width):
+        for y in range(height):
             if fileName[x,y] < minValue:
                 minValue = fileName[x,y]
             if fileName[x,y] > maxValue:
                 maxValue = fileName[x,y]
-    """
     
+    #finds values for horizontal midpoint
     for x in range(width):
-        pixelValues.append(fileName[x,y])
-                
-    return minValue, maxValue, pixelValues  
+        horizontalMidpointArray.append(fileName[x, horizontalMidpoint])
+    
+    #finds values for vertical midpoint
+    for y in range(height):
+        verticalMidpointArray.append(fileName[verticalMidpoint, y])    
+    
+    return minValue, maxValue, horizontalMidpointArray, verticalMidpointArray
 
-def blurring(imag):
-    blurred = ndimage.gaussian_filter(imag, 5)
+def plotTable(yAxisData):
+    numPoints = len(yAxisData)
+    xAxisData = []
+    
+    #increment by 1 across x-axis
+    for i in range(numPoints):
+        xAxisData.append(i+1)        
+
+    matplotlib.pyplot.scatter(xAxisData, yAxisData)    
+    pylab.show()
+
+def blurring(image):
+    blurred = ndimage.gaussian_filter(image, 5)
     # maxBright = -1
     # brightx = 0
     # brighty = 0
@@ -98,8 +103,8 @@ def blurring(imag):
     # print maxBright
     return blurred
 
-def drawContour(imag):
-    print countour
+def drawContour(image):
+    print contour
 
     
 if __name__ == '__main__':
